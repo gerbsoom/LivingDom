70fb68aff5486c0fd0fcf98651f1b92064897549
gerbsoom <gerbsoom@web.de>
2015-03-01 05:43:35 +0100

    Nice-up first version with documentation and debug functionality.

--- js/DomNode.js
+++ js/DomNode.js
@@ -6,39 +6,43 @@
  * @author Markus Riegert <desmodul@drow-land.de>
  */
 
-var debugMode = "normal";
-
 /**
- * Sets the debugmode with which the application logs about modifying the DomNodes.
- * Possible values are:
- * - silent:  Suppressing output completely to gain maximum render performance
- * - normal:  Noticeable Events (INFO), warnings and errors will be reported
- * - verbose: All log possibilities will be executed and reported in detail
+ * Constructs a DomNode from the provided input parameters.
  *
- * @param string _newDebugMode The new debug mode.
+ * @param _domNodeId Unique ID to identify the HTMLElement.
+ * @param _domNodeContent The raw HTML content of the DomNode.
+ * @param _parentDomNode The parent DomNode which holds the reference of this node and spawned it into the DOM.
+ * @constructor
  */
-function setDebugLevel(_newDebugMode)
+function DomNode(_domNodeId, _domNodeContent, _parentDomNode)
 {
-    debugMode = _newDebugMode
-}
+    /** @var Unique ID to identify the HTMLElement */
+    var domNodeId = _domNodeId;
 
-function DomNode(_domNodeContent, _parentDomNode)
-{
+    /** @var HTMLElement domNodeContent */
     var domNodeContent = _domNodeContent;
+    /** @var DomNode parentDomNode */
     var parentDomNode = _parentDomNode;
+
+    /** @var DomNode[] domNodeChildren */
     var domNodeChildren = [];
     var isRootNode = false;
 
-    if (parentDomNode instanceof DomNode)
-    {
-        addElementToComponent(parentDomNode.getDomNodeContent(), domNodeContent);
-    }
-    else
-    {
+    if (!(parentDomNode instanceof DomNode))
+    {// no parent DomNode means we are a root node
         isRootNode = true;
         document.body.appendChild(domNodeContent);
     }
-    domNodeChildren.push(domNodeContent);
+
+    /**
+     * Returns the unique ID of this DomNode.
+     *
+     * @returns The unique ID of this DomNode.
+     */
+    this.getDomNodeId = function()
+    {
+        return domNodeId;
+    };
 
     /**
      * Sets the provided DomElement overwriting existing ones.
@@ -49,7 +53,10 @@ function DomNode(_domNodeContent, _parentDomNode)
      */
     this.setDomNodeChildren = function(_domNodeChildren)
     {
-        console.log("[DOM-NODE] setDomNodeChildren()", _domNodeChildren);
+        if (matchesDebugMode("debug"))
+        {
+            console.log("[DOM-NODE] setDomNodeChildren()", _domNodeChildren);
+        }
         domNodeChildren = _domNodeChildren;
     };
 
@@ -60,50 +67,112 @@ function DomNode(_domNodeContent, _parentDomNode)
      */
     this.addDomNodeChildren = function(_domNodeChildren)
     {
-        console.log("[DOM-NODE] addDomNodeChildren()", _domNodeChildren);
+        if (matchesDebugMode("debug"))
+        {
+            console.log("[DOM-NODE] addDomNodeChildren()", _domNodeChildren);
+        }
         domNodeChildren.push(_domNodeChildren);
         addElementToComponent(domNodeContent, _domNodeChildren.getDomNodeContent());
     };
 
+    /**
+     * Returns all DomNode objects from the list of children.
+     *
+     * @returns DomNode[] Contains all DomNode objects from the list of children.
+     */
     this.getDomNodeChildren = function()
     {
-        console.log("[DOM-NODE] getDomNodeChildren()", domNodeChildren);
+        if (matchesDebugMode("debug"))
+        {
+            console.log("[DOM-NODE] getDomNodeChildren()", domNodeChildren);
+        }
         return domNodeChildren;
     };
 
+    /**
+     * Returns the parent DomNode which holds the reference to this DomNode.
+     *
+     * @returns DomNode The parent DomNode if it exist, otherwise null.
+     */
     this.getParentDomNode = function()
     {
-        console.log("[DOM-NODE] getParentDomNode()", parentDomNode);
+        if (matchesDebugMode("debug"))
+        {
+            console.log("[DOM-NODE] getParentDomNode()", parentDomNode);
+        }
         return parentDomNode;
     };
 
+    /**
+     * Replaces the set parent DomNode with a new one and triggers all rendering events on the DOM.
+     *
+     * @param _parentDomNode The parent DomNode if it exist, otherwise null.
+     */
     this.setParentDomNode = function(_parentDomNode)
     {
-        console.log("[DOM-NODE] setParentDomNode()", _parentDomNode);
+        if (matchesDebugMode("debug"))
+        {
+            console.log("[DOM-NODE] setParentDomNode()", _parentDomNode);
+        }
         removeElementFromComponent(parentDomNode.getDomNodeContent(), domNodeContent);
         addElementToComponent(_parentDomNode.getDomNodeContent(), domNodeContent);
         parentDomNode = _parentDomNode;
     };
 
+    /**
+     * Returns the configured HTMLElement of this DomNode.
+     *
+     * @returns The configured HTMLElement of this DomNode.
+     */
     this.getDomNodeContent = function()
     {
-        console.log("[DOM-NODE] getDomNodeContent()", domNodeContent);
+        if (matchesDebugMode("debug"))
+        {
+            console.log("[DOM-NODE] getDomNodeContent()", domNodeContent);
+        }
         return domNodeContent;
     };
 
+    /**
+     * Replaces the set DomNode content with a new one and triggers all rendering events on the DOM.
+     *
+     * @param _domNodeContent The HTMLElement which replaces hte configured .
+     */
     this.setDomNodeContent = function(_domNodeContent)
     {
-        console.log("[DOM-NODE] setDomNodeContent()", _domNodeContent);
+        if (matchesDebugMode("debug"))
+        {
+            console.log("[DOM-NODE] setDomNodeContent()", _domNodeContent);
+        }
         domNodeContent = _domNodeContent;
         domNodeChildren = [];
         setChildForComponent(parentDomNode.getDomNodeContent(), domNodeContent);
         this.setDomNodeChildren(domNodeContent.getDomNodeChildren());
     };
 
+    /**
+     * Returns if this DomNode is a root node.
+     *
+     * @returns bool True means that this DomNode is a root node.
+     */
     this.isRoot = function()
     {
-        console.log("[DOM-NODE] isRootNode()", isRootNode);
+        if (matchesDebugMode("debug"))
+        {
+            console.log("[DOM-NODE] isRootNode()", isRootNode);
+        }
         return isRootNode;
     };
+
+    /**
+     * Debugs detail informatioon about the state of this DomNode.
+     */
+    this.debugDomNode = function()
+    {
+        if (matchesDebugMode("debug"))
+        {
+
+        }
+    }
 
 }
--- js/library.js
+++ js/library.js
@@ -6,24 +6,64 @@
  * @author Markus Riegert <desmodul@drow-land.de>
  */
 
+/**
+ * Core library functions to manipulate the DOM and generate HTMLElements.
+ */
+
+/**
+ * Adds the provided element to the requested component.
+ *
+ * @param _component The component where the provided element gets added.
+ * @param _element The element that gets added to the component.
+ */
 function addElementToComponent(_component, _element)
 {
-    console.log("[LIBRARY] addElementToComponent()", _component, _element);
+    if (matchesDebugMode("debug"))
+    {
+        console.log("[LIBRARY] addElementToComponent()", _component, _element);
+    }
     _component.appendChild(_element);
 }
 
+/**
+ * Removes the provided element from the requested component.
+ *
+ * @param _component The component from where the provided element gets removed.
+ * @param _element The element that gets deleted from the component.
+ */
 function removeElementFromComponent(_component, _element)
 {
-    console.log("[LIBRARY] removeElementFromComponent()", _component, _element);
+    if (matchesDebugMode("debug"))
+    {
+        console.log("[LIBRARY] removeElementFromComponent()", _component, _element);
+    }
     _component.removeChild(_element);
 }
 
+/**
+ * Sets the provided element as children to the requested component.
+ *
+ * @param _component The component where the provided element gets set as children.
+ * @param _element The element that gets set as children to the component.
+ */
 function setChildForComponent(_component, _element)
 {
-    console.log("[LIBRARY] setChildForComponent()", _component, _element);
+    if (matchesDebugMode("debug"))
+    {
+        console.log("[LIBRARY] setChildForComponent()", _component, _element);
+    }
     _component.setChildren(_element);
 }
 
+/**
+ * Generates a DIV container for the provided parameters.
+ *
+ * @param _class Sets the class of the generated DIV container.
+ * @param _tagId Sets the tagID of the generated DIV container.
+ * @param _innerText Adds an inner text into the  DIV container.
+ * @param _style Adds an stylesheet string into the  DIV container.
+ * @returns {HTMLElement} The generated DIV container as HTMLElement.
+ */
 function generateDiv(_class, _tagId, _innerText, _style)
 {
     var newDiv = document.createElement('div');
